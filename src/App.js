69cbdb152f990ae2aff37208725e4c0e2d336169
tugba-koc <koctug@itu.edu.tr>
2022-01-10 13:20:12 +0300

    context added to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Index";
+import { ResultProvider } from "./context/ResultContext";
 
 function App() {
-  return <div>
-    <Navbar />
-  </div>;
+  return (
+    <div>
+      <ResultProvider>
+        <Navbar />
+      </ResultProvider>
+    </div>
+  );
 }
 
 export default App;
