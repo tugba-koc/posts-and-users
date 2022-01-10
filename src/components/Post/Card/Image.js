68c24f648b96ad8e9e107ca257302795058a73d5
tugba-koc <koctug@itu.edu.tr>
2022-01-10 14:18:56 +0300

    image size changed

--- src/components/Post/Card/Image.js
+++ src/components/Post/Card/Image.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Image({post}) {
     return ( 
         <>
-           <img className="card-image" src={post.image} alt="" /> 
+           <img className="card-image w-100" src={post.image} alt="" /> 
         </>
     )
 }
