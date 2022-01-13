4a39e73ae1a195292cf96e23e07aa6fc069a3d7e
tugba-koc <koctug@itu.edu.tr>
2022-01-13 14:25:14 +0300

    link to userDetail page designed

--- src/components/Post/PostItem/PostItem.js
+++ src/components/Post/PostItem/PostItem.js
@@ -6,16 +6,17 @@ import Likes from "../Card/Likes";
 import PublishedDate from "../Card/PublishedDate";
 import PubImage from "../Publisher/PubImage";
 import PubText from "../Publisher/PubText";
+import { Link } from "react-router-dom";
 
 function PostItem({ post }) {
   return (
     <div className="bg-light p-2">
-      <div className="d-flex flex-row mb-1 ">
+      <Link className="d-flex flex-row mb-1" to="/">
         <PubImage owner={post.owner} />
         <div className="ms-2">
           <PubText owner={post.owner} />
         </div>
-      </div>
+      </Link>
 
       <Image post={post} />
       <div className="mt-2">
