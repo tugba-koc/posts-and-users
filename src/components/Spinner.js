184cc5e68610fa86d78631b70759d45f97ab1359
tugba-koc <koctug@itu.edu.tr>
2022-01-14 11:34:13 +0300

    align center

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Spinner() {
   return (
-    <div class="d-flex justify-content-center vh-100">
+    <div class="d-flex justify-content-center vh-100 align-items-center">
       <div class="spinner-border" role="status">
         <span class="visually-hidden">Loading...</span>
       </div>
