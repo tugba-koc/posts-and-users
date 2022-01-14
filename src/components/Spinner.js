8a7525cdb86355550d3b517ed99c1ad14d4b825a
tugba-koc <koctug@itu.edu.tr>
2022-01-14 11:25:23 +0300

    isLoading and Spinner relation was done

---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Spinner() {
+  return (
+    <div class="d-flex justify-content-center vh-100">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
