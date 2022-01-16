434c35faacdf148e8d023faa84abfef8054e1d2a
tugba-koc <koctug@itu.edu.tr>
2022-01-16 19:56:20 +0300

    context removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Index";
-// import { ResultProvider } from "./context/ResultContext";
 
 function App() {
   return (
     <div className="main min-vh-100">
-      {/* <ResultProvider> */}
         <Navbar />
-      {/* </ResultProvider> */}
     </div>
   );
 }
